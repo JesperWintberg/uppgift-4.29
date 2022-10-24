b48648f9e7a2d1427506585a7e74409ce5f5302b
JesperWintberg <111962418+JesperWintberg@users.noreply.github.com>
2022-10-24 19:27:53 +0200

    Add project files.

---
+++ uppgift 4.29/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur hög vill du ha din rätvinkliga triangel");
+            string höjd = Console.ReadLine();
+
+            int höjd2 = int.Parse(höjd);
+
+            for (int i = 0; i < höjd2; i++)
+            {
+                Console.WriteLine();
+
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
